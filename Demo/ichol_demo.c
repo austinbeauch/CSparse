04f957fbc26c2157b6b342e4ac2746606ce1d38b
Austin Beauchamp <austinbeauch@gmail.com>
2022-05-27 18:37:40 -0700

    Add left looking cholesky with big memory leak

--- Demo/ichol_demo.c
+++ Demo/ichol_demo.c
@@ -31,6 +31,12 @@ void printv(dvec *arr, int num){
     printf("\n");
 }
 
+void printcs(cs *A, int num){
+    print(A->x, num);
+    printi(A->i, num);
+    printi(A->p, num);
+}
+
 csi cs_utsolve_new (const cs *U, dvec *x)
 {
     csi p, j, n, *Up, *Ui ;
@@ -66,6 +72,64 @@ int compare_indices(const void *a, const void *b)
     if(a1->index > a2->index) return 1;
     if(a2->index < a2->index) return -1;
     return 0;
+}
+
+cs *add_vecs(cs *A, cs *B, float alpha, float beta){
+    cs *C;
+    double *Ax, *Bx, *Cx;
+    csi *Ai, *Ap, *Bp, *Bi, *Cp, *Ci;
+    int acount = 0, bcount = 0, ccount = 0;
+    Bp = B->p ; Bi = B->i ; Bx = B->x ;
+    Ap = A->p ; Ai = A->i ; Ax = A->x ;
+
+    C = cs_spalloc (A->m, 1, A->m, 1, 0); 
+    Cp = C->p ; Ci = C->i ; Cx = C->x ;
+
+    while (acount < Ap[1] || bcount < Bp[1] )
+    {
+
+        if (acount == Ap[1])
+        {
+            Cx[ccount] = beta * Bx[bcount];
+            Ci[ccount] = Bi[bcount];
+            bcount ++;
+        }
+        else if (bcount == Bp[1])
+        {
+            Cx[ccount] = alpha * Ax[acount];
+            Ci[ccount] = Ai[acount];
+            acount ++;
+        }
+
+        else if (Ai[acount] == Bi[bcount])
+        {
+            Cx[ccount] = alpha * Ax[acount] +  beta * Bx[bcount];
+            Ci[ccount] = Ai[acount];
+            acount ++;
+            bcount ++;
+        }
+
+        // I can't merge these two if statements with above and I don't know why
+        else if (Ai[acount] < Bi[bcount])
+        {
+            Cx[ccount] = alpha * Ax[acount];
+            Ci[ccount] = Ai[acount];
+            acount ++;
+        }
+
+        else if (Bi[bcount] < Ai[acount])
+        {
+            Cx[ccount] = beta * Bx[bcount];
+            Ci[ccount] = Bi[bcount];
+            bcount ++;
+        }
+        
+        ccount ++;
+        
+    }
+    Cp[0] = 0; Cp[1] = ccount;
+
+    return C ;
 }
 
 cs *cs_ichol (const cs *A, float t, csi max_p)
@@ -138,15 +202,17 @@ cs *cs_ichol (const cs *A, float t, csi max_p)
             // first sort all values
             qsort(x_vec, x_nnz, sizeof(dvec), compare_values);
 
-            // then sort the x_nnz p by index for inserting into Lx/Li in order
+            // then sort the biggest p values by index for inserting into Lx/Li in order
             qsort(x_vec, keep_vals, sizeof(dvec), compare_indices);
         }
 
         // store x_nnz entries in Lx and Li
         for (i = 0; i < keep_vals; i++){
             Lx[xcount] = x_vec[i].data;
-            l12 += x_vec[i].data * x_vec[i].data; // store dot product
             Li[xcount++] = x_vec[i].index;
+
+            // store dot product
+            l12 += x_vec[i].data * x_vec[i].data; 
         }
 
         // compute diagonal entry in L
@@ -156,15 +222,118 @@ cs *cs_ichol (const cs *A, float t, csi max_p)
             return L;
         }
 
+        // store diagonal and update end column pointer
         Lx[xcount] = sqrt(Akk - l12);
         Li[xcount++] = k;
-        Lp[pcount++] = Lp[pcount-1] + keep_vals + 1;
+        Lp[pcount++] = Lp[pcount-1] + keep_vals + 1; // or just xcount?
 
     }
 
     L->n = A->n; L->m = A->n;
+    // TODO: free some stuff as well
     return L;
 }
+
+cs *cs_ichol_left (const cs *A, float t, csi max_p)
+{
+    // printf("START\n");
+    double *Lx, *Ax, *x, Akk, l12;
+    csi x_nnz, i, p, k, n, *Li, *Lp, *Ap, *Ai, icount = 0, xcount = 0, pcount = 1 ;
+    cs *L, *B, *C;
+    if (!CS_CSC (A)) return (NULL) ;
+    n = A->n ;
+    Ap = A->p ; Ai = A->i ; Ax = A->x ;
+    dvec* x_vec = malloc(n * sizeof *x_vec);
+    max_p = max_p < n ? max_p : n;
+    csi maxvals = (max_p * (max_p+1) / 2) + (max_p * (n-max_p));
+
+    L = cs_spalloc (n, n, maxvals, 1, 0); 
+    Lp = L->p ; Li = L->i ; Lx = L->x ;
+    Lp[0] = 0;
+
+    double *Bx, *Cx;
+    csi *Bp, *Bi, *Cp, *Ci;
+    B = cs_spalloc (n, 1, n, 1, 0); 
+    Bp = B->p ; Bi = B->i ; Bx = B->x ;
+    Bp[0] = 0;
+
+    C = cs_spalloc (n, 1, n, 1, 0); 
+    Cp = C->p ; Ci = C->i ; Cx = C->x ;
+    Cp[0] = 0;
+
+    csi bcount = 0;
+
+    for (k = 0 ; k < n ; k++) { 
+
+        // for (i=0; i<n; i++) Bx[i] = 0;  Bi[i] = 0;
+        
+        Bp[0] = 0;
+        Bp[1] = 0;
+
+        bcount = 0;
+        // grab kth lower column from A
+        B->m = n-k; // set to a lenght k+1 column vector
+        for (p = Ap [k] ; p < Ap [k+1] ; p++) {
+            if (Ai[p] < k) continue;
+            Bx[bcount]   = Ax[p];
+            Bi[bcount++] = Ai[p] - k;
+        }
+        Bp[1] = bcount;
+
+        // find nonzero entries in the kth row of L
+        for (int search_col = 0; search_col < k; search_col++){
+            for (p = Lp [search_col] ; p < Lp [search_col+1] ; p++){
+                if (Li[p] > k) break; // we're past the kth row with no entry, break
+
+                else if (Li[p] == k) {
+                    // we found an existing j
+                    float L_jk = Lx[p];
+
+                    // need to grab the rest of the column, downwards from p, store in vector C
+                    C->m = n-k;
+                    int ccount = 0;
+                    for (int pp = p ; pp < Lp [search_col+1] ; pp++) {
+                        Cx[ccount]   = Lx[pp] * L_jk; // do multiplication with L_jk
+                        Ci[ccount++] = Li[pp] - k;
+                    }
+                    Cp[1] = ccount;
+                    // printf("k=%ld\n", k);
+                    B = add_vecs(B, C, 1, -1);
+                    // B = cs_add(B, C, 1, -1);
+                    Bp = B->p ; Bi = B->i ; Bx = B->x ;
+                    break;
+                }
+            }
+        }
+
+        // set diagonal
+        if (Bx[0] <= 0)
+        {
+            printf("Not positive definite");
+            return L;
+        }
+        
+        Lx[xcount] = sqrt(Bx[0]);
+        Li[xcount++] = Bi[0] + k;
+
+        // set column
+        for (p = Bp [0]+1 ; p < Bp [1] ; p++)
+        {
+            float val = Bx[p] / sqrt(Bx[0]);
+            if (fabs(val) > t)
+            {
+                Lx[xcount]   = val;
+                Li[xcount++] = Bi[p] + k;
+            }
+
+        }
+
+        Lp[pcount++] = xcount;
+
+    }
+    return L;
+}
+
 
 int main (void)
 {
@@ -177,11 +346,14 @@ int main (void)
     double cpu_time_used;
 
     FILE *fp;
-    // stdin = fopen("../Matrix/eu3_2_0", "rb+");
+    stdin = fopen("../Matrix/eu3_2_0", "rb+");
     // stdin = fopen("../Matrix/eu3_22_0", "rb+");
+    // stdin = fopen("../Matrix/eu3_100_0", "rb+");
     // stdin = fopen("../Matrix/dense_rand", "rb+");
     // stdin = fopen("../Matrix/triplet_mat", "rb+");
-    stdin = fopen("../Matrix/manual_8x8", "rb+");
+    // stdin = fopen("../Matrix/manual_8x8", "rb+");
+    // stdin = fopen("../Matrix/A5x5", "rb+");
+
     T = cs_load(stdin) ;
     A = cs_compress (T) ;              
     // cs_print (A, 0) ; /* print A */
@@ -189,21 +361,28 @@ int main (void)
     n = A->n ;
     printf("n = %li\n", n);
 
-    start = clock();
-    S = cs_schol (order, A) ;               /* ordering and symbolic analysis */
-    N = cs_chol (A, S) ;                    /* numeric Cholesky factorization */
-    // printf ("chol(L):\n") ; cs_print (cs_transpose(N->L, 1), 0) ;
-    end = clock();
-    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;  
-    printf("chol CPU Time: %f\n", cpu_time_used);
+    // start = clock();
+    // S = cs_schol (order, A) ;               /* ordering and symbolic analysis */
+    // N = cs_chol (A, S) ;                    /* numeric Cholesky factorization */
+    // // printf ("chol(L):\n") ; cs_print (N->L, 0) ;
+    // // printf ("chol(L):\n") ; cs_print (cs_transpose(N->L, 1), 0) ;
+    // end = clock();
+    // cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;  
+    // printf("full chol CPU Time: %f\n", cpu_time_used);
+
+    // start = clock();
+    float t = 0; int p = n;
+    // L = cs_ichol(A, t, n);
+    // end = clock();
+    // cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    // printf("up chol CPU Time: %f\n", cpu_time_used);
 
     start = clock();
-    float t = 0.2; int p = 4;
-    L = cs_ichol(A, t, p);
-    printf ("ichol(L, %e, %d):\n", t, p) ; cs_print (L, 0) ;
+    t = 0; p = n;
+    L = cs_ichol_left(A, t, n);
     end = clock();
     cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-    printf("ichol CPU Time: %f\n", cpu_time_used);
+    printf("left chol CPU Time: %f\n", cpu_time_used);
 
     // printf ("ichol(L, %e, %d):\n", t, p) ; cs_print (L, 0) ;
 
